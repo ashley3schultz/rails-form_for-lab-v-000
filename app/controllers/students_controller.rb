bffa13c159cec4d9a3c8d5cc0d622879529392f0
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-28 00:05:02 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,26 @@
 class StudentsController < ApplicationController
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:first_name).require(:last_name, :last_name))
+    redirect_to school_class_path(@student)
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(params.require(:school_class).permit(:title, :room_number))
+    redirect_to school_class_path(@student)
+  end
+
 end
